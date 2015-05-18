c0726c4210ea5d69111b310748341d592b4dd5d3
kriminal666 <kriminalaverno@gmail.com>
2015-05-18 17:14:45 +0200

    added grade scale

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ This is a evaluation module RestAPI administrator. [http://ng-admin.marmelab.com
 
 The interesting part of the code is in public/app.js
 
+## Configuration
+
+You have to disable middleware on the controller constructor (Evaluation and GradeScale). Comment this line:
+
+```php
+$this->middleware('auth');
+```
+Now change de return of the index and show methods:
+
+Index:
+
+```php
+$gradeScales = GradeScale::all();
+  return $this->gradeScaleTransformer->transformCollection($gradeScales->toArray());
+```
+
+Show($id):
+
+```php
+ $evaluation = Evaluation::find($id);
+ return $this->evaluationTransformer->transform($evaluation);
+```
+
+
 ## Installation
 
 ```sh
@@ -19,3 +43,12 @@ make run
 ```
 
 You can now open `http://localhost:3000/`
+
+![alt text][evaluation.png]
+
+![alt text][grade_scale.png]
+
+## Evaluation project
+
+[Here is the project](https://github.com/kriminal666/Evaluation)
+
--- public/app.js
+++ public/app.js
@@ -28,28 +28,25 @@
             .infinitePagination(true) // load pages as the user scrolls
             .fields([
                 nga.field('evaluationId').label('Id'), // The default displayed name is the camelCase field name. label() overrides id
-                nga.field('academicPeriodId'), // the default list field type is "string", and displays as a string
-                nga.field('studentId'), // the default list field type is "string", and displays as a string
-                nga.field('subModuleId'),
-                nga.field('markId'),
+                nga.field('academicPeriodId', 'number'), // the default list field type is "string", and displays as a string
+                nga.field('studentId', 'number'), // the default list field type is "string", and displays as a string
+                nga.field('subModuleId', 'number'),
+                nga.field('markId', 'number'),
                 nga.field('creationDate'),
-                nga.field('creationUserId'),
+                nga.field('creationUserId', 'number'),
                 nga.field('updateDate'),
-                nga.field('lastUpdateUserId'),
+                nga.field('lastUpdateUserId', 'number'),
                 nga.field('deleted_at'),
                 
             ])
             .listActions(['show', 'edit', 'delete']);
         evaluation.creationView().fields(
             [
-                nga.field('evaluation_Id') // the default edit field type is "string", and displays as a text input
-                    .attributes({ placeholder: 'the post title' }) // you can add custom attributes, too
-                    .validation({ required: true, minlength: 3, maxlength: 100 }), // add validation rules for fields
-                nga.field('markId'), // the default list field type is "string", and displays as a string
-                nga.field('academicPeriodId'), // the default list field type is "string", and displays as a string
-                nga.field('subModuleId'),
-                nga.field('studentId'),
-                nga.field('lastUpdateUserId'),
+                nga.field('markId', 'number'), // the default list field type is "string", and displays as a string
+                nga.field('academicPeriodId', 'number'), // the default list field type is "string", and displays as a string
+                nga.field('subModuleId', 'number'),
+                nga.field('studentId', 'number'),
+                nga.field('lastUpdateUserId', 'number'),
             ]
         );
 
@@ -63,6 +60,53 @@
         evaluation.showView() // a showView displays one entry in full page - allows to display more data than in a a list
             .fields([
                 nga.field('evaluationId'),
+                evaluation.editionView().fields(), // reuse fields from another view in another order
+            ]);
+
+             var gradeScale = nga.entity('gradescales').identifier(nga.field('gradeScaleId'));
+        app.addEntity(gradeScale);
+
+        // set the list of fields to map in each post view
+        gradeScale.dashboardView().fields(/* see example below */);
+
+        gradeScale.dashboardView() // customize the dashboard panel for this entity
+            .title('Recent Grade Scales')
+            .order(1) // display the post panel first in the dashboard
+            .perPage(20) // limit the panel to the 20 latest posts
+            .fields([nga.field('gradeScaleId').isDetailLink(true)]);
+
+        gradeScale.listView()
+            .title('All grade scales') // default title is "[Entity_name] list"
+            .description('List of grade scale with infinite pagination') // description appears under the title
+            .infinitePagination(true) // load pages as the user scrolls
+            .fields([
+                nga.field('gradeScaleId').label('Id'), // The default displayed name is the camelCase field name. label() overrides id
+                nga.field('description'), // the default list field type is "string", and displays as a string
+                nga.field('creationDate'),
+                nga.field('creationUserId', 'number'),
+                nga.field('updateDate'),
+                nga.field('lastUpdateUserId', 'number'),
+                nga.field('deleted_at'),
+                
+            ])
+            .listActions(['show', 'edit', 'delete']);
+        gradeScale.creationView().fields(
+            [
+                nga.field('description'), // the default list field type is "string", and displays as a string
+                nga.field('lastUpdateUserId', 'number'),
+            ]
+        );
+
+        gradeScale.editionView()
+            .title('Edit grade scale "{{ entry.values.gradeScaleId }}"') // title() accepts a template string, which has access to the entry
+            .actions(['list', 'show', 'delete']) // choose which buttons appear in the top action bar. Show is disabled by default
+            .fields([
+                evaluation.creationView().fields(), // fields() without arguments returns the list of fields. That way you can reuse fields from another view to avoid repetition
+            ]);
+
+        gradeScale.showView() // a showView displays one entry in full page - allows to display more data than in a a list
+            .fields([
+                nga.field('gradeScaleId'),
                 evaluation.editionView().fields(), // reuse fields from another view in another order
             ]);
 
